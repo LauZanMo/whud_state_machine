2c10422450df8cdc3cac2020015ce5b1cea8e2a2
LauZanMo <LauZanMo@whu.edu.cn>
2021-06-03 13:03:17 +0800

    fix whud_basic_control plugin and config files bug

--- src/plugins/WhudBasicControl.cpp
+++ src/plugins/WhudBasicControl.cpp
@@ -11,9 +11,10 @@ using namespace std;
 namespace whud_state_machine {
 class WhudBasicControl : public PluginBase {
 public:
-  WhudBasicControl() : PluginBase(), sm_nh_("~basic_control"){};
+  WhudBasicControl() : PluginBase(), nh_("~basic_control"){};
 
   enum Command { NONE = 0, TAKEOFF, LAND, HEIGHT_CONTROL, YAW_CONTROL };
+
   void OnInit(MavRosPublisher &mavros_pub) {
     mavros_pub_ = &mavros_pub;
   }
@@ -21,38 +22,38 @@ public:
   bool SetTask(ros::V_string param) {
     PluginBase::SetTask(param);
     if (param[0] == "takeoff") {
-      takeoff.data.clear();
+      takeoff_.data.clear();
       // set z axis speed
-      takeoff.data.push_back(atof(param[1].c_str()));
+      takeoff_.data.push_back(atof(param[1].c_str()));
 
       // set height
-      takeoff.data.push_back(atof(param[2].c_str()));
+      takeoff_.data.push_back(atof(param[2].c_str()));
 
       command_ = Command::TAKEOFF;
 
     } else if (param[0] == "land") {
       // set z axis speed
-      land.data = atof(param[1].c_str());
+      land_.data = atof(param[1].c_str());
 
       command_ = Command::LAND;
 
     } else if (param[0] == "height_control") {
-      height_control.data.clear();
+      height_control_.data.clear();
       // set z axis speed
-      height_control.data.push_back(atof(param[1].c_str()));
+      height_control_.data.push_back(atof(param[1].c_str()));
 
       // set target height(relatively)
-      height_control.data.push_back(atof(param[2].c_str()));
+      height_control_.data.push_back(atof(param[2].c_str()));
 
       command_ = Command::HEIGHT_CONTROL;
 
     } else if (param[0] == "yaw_control") {
-      yaw_control.data.clear();
+      yaw_control_.data.clear();
       // set target angle
-      yaw_control.data.push_back(atof(param[1].c_str()));
+      yaw_control_.data.push_back(atof(param[1].c_str()));
 
       // 0: absolute, 1: relative
-      yaw_control.data.push_back(atof(param[2].c_str()));
+      yaw_control_.data.push_back(atof(param[2].c_str()));
 
       command_ = Command::YAW_CONTROL;
 
@@ -62,66 +63,55 @@ public:
   }
 
   virtual void TaskSpin() override {
-    sm_nh_.getParam("/mavros/whud_basic/ack_cmd_index", mavros_command_);
-    sm_nh_.getParam("/mavros/whud_basic/ack_result", mavros_result_);
+    if (mavros_pub_ != nullptr && control_flag_ == true) {
+      int mavros_command, mavros_result;
+      nh_.getParam("/mavros/whud_basic/ack_cmd_index", mavros_command);
+      nh_.getParam("/mavros/whud_basic/ack_result", mavros_result);
 
-    switch (command_) {
-    case Command::TAKEOFF:
-      if (mavros_command_ == 24 && mavros_result_ == 0) {
-        task_status_ = TaskStatus::DONE;
-      } else if (mavros_command_ != 24 && mavros_result_ != 5) {
-        if (mavros_pub_ != nullptr && control_flag_ == true) {
-          mavros_pub_->takeoff_pub.publish(takeoff);
-        }
+      switch (command_) {
+      case Command::TAKEOFF:
+        if (mavros_command == 24 && mavros_result == 0)
+          task_status_ = TaskStatus::DONE;
+        else if (mavros_command != 24 && mavros_result != 5)
+          mavros_pub_->takeoff_pub.publish(takeoff_);
+        break;
+
+      case Command::LAND:
+        if (mavros_command == 23 && mavros_result == 0)
+          task_status_ = TaskStatus::DONE;
+        else if (mavros_command != 23 && mavros_result != 5)
+          mavros_pub_->land_pub.publish(land_);
+        break;
+
+      case Command::HEIGHT_CONTROL:
+        if (mavros_command == 113 && mavros_result == 0)
+          task_status_ = TaskStatus::DONE;
+        else if (mavros_command != 113 && mavros_result != 5)
+          mavros_pub_->height_pub.publish(height_control_);
+        break;
+
+      case Command::YAW_CONTROL:
+        if (mavros_command != 115 && mavros_result == 0)
+          task_status_ = TaskStatus::DONE;
+        else if (mavros_command == 115 && mavros_result != 5)
+          mavros_pub_->yaw_pub.publish(yaw_control_);
+        break;
+
+      default:
+        break;
       }
-      break;
-    case Command::LAND:
-      if (mavros_command_ == 23 && mavros_result_ == 0) {
-        task_status_ = TaskStatus::DONE;
-      } else if (mavros_command_ != 23 && mavros_result_ != 5) {
-        if (mavros_pub_ != nullptr && control_flag_ == true) {
-          mavros_pub_->land_pub.publish(land);
-        }
-      }
-      break;
-    case Command::HEIGHT_CONTROL:
-      if (mavros_command_ == 113 && mavros_result_ == 0) {
-        task_status_ = TaskStatus::DONE;
-      } else if (mavros_command_ != 113 && mavros_result_ != 5) {
-        if (mavros_pub_ != nullptr && control_flag_ == true) {
-          mavros_pub_->height_pub.publish(height_control);
-        }
-      }
-      break;
-    case Command::YAW_CONTROL:
-      if (mavros_command_ != 115 && mavros_result_ == 0) {
-        task_status_ = TaskStatus::DONE;
-      } else if (mavros_command_ == 115 && mavros_result_ != 5) {
-        if (mavros_pub_ != nullptr && control_flag_ == true) {
-          mavros_pub_->yaw_pub.publish(yaw_control);
-        }
-      }
-      break;
-    default:
-      break;
     }
   }
 
   virtual void StopTask() override {}
 
 private:
-  ros::NodeHandle sm_nh_;
-  std_msgs::Float64MultiArray takeoff;
-  float takeoff_params[2];
-  std_msgs::Float64 land;
-  std_msgs::Float64MultiArray height_control;
-  float height_control_params[2];
-  std_msgs::Float64MultiArray yaw_control;
-  float yaw_control_params[2];
+  ros::NodeHandle nh_;
   Command command_;
-
-  int mavros_command_;
-  int mavros_result_;
+  std_msgs::Float64MultiArray takeoff_;
+  std_msgs::Float64 land_;
+  std_msgs::Float64MultiArray height_control_;
+  std_msgs::Float64MultiArray yaw_control_;
 };
 
 }  // namespace whud_state_machine
